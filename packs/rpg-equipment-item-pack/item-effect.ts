1924181fbe5dd2295b1fb17fecf419452cb933ad
Agadar <martin.donkersloot@outlook.com>
2023-06-23 03:13:55 +0200

    Rebalancing rpg items

--- packs/rpg-equipment-item-pack/item-effect.ts
+++ packs/rpg-equipment-item-pack/item-effect.ts
@@ -37,11 +37,11 @@ export class ItemEffect {
         // Core bot
 
         ["Devout", new ItemEffect("Devout", AlterUserScoreArgs.DANKTIMESBOT_ORIGIN_NAME,
-            [AlterUserScoreArgs.NORMAL_DANKTIME_REASON], 0.1,
+            [AlterUserScoreArgs.NORMAL_DANKTIME_REASON], 0.2,
             "Increases normal dank time points received by", 4)],
 
         ["Sentinel", new ItemEffect("Sentinel", AlterUserScoreArgs.DANKTIMESBOT_ORIGIN_NAME,
-            [AlterUserScoreArgs.RANDOM_DANKTIME_REASON], 0.15,
+            [AlterUserScoreArgs.RANDOM_DANKTIME_REASON], 0.25,
             "Increases random dank time points received by", 4)],
 
         ["Eternal", new ItemEffect("Eternal", AlterUserScoreArgs.DANKTIMESBOT_ORIGIN_NAME,
@@ -50,29 +50,29 @@ export class ItemEffect {
 
         // Life
 
-        ["Pious", new ItemEffect("Pious", ItemEffect.LIFE_PLUGIN, [ItemEffect.WORK_COMPLETED], 0.1,
+        ["Pious", new ItemEffect("Pious", ItemEffect.LIFE_PLUGIN, [ItemEffect.WORK_COMPLETED], 0.25,
             "Increases points gained from working by", 4)],
 
-        ["Mastermind", new ItemEffect("Mastermind", ItemEffect.LIFE_PLUGIN, [ItemEffect.CRIME_COMMITTED], 0.1,
+        ["Mastermind", new ItemEffect("Mastermind", ItemEffect.LIFE_PLUGIN, [ItemEffect.CRIME_COMMITTED], 0.25,
             "Increases points gained from committing crimes by", 4)],
 
-        ["Succubus", new ItemEffect("Succubus", ItemEffect.LIFE_PLUGIN, [ItemEffect.BRIBE], -0.2,
+        ["Succubus", new ItemEffect("Succubus", ItemEffect.LIFE_PLUGIN, [ItemEffect.BRIBE], -0.25,
             "Reduces the cost of bribes by")],
 
-        ["Liberator", new ItemEffect("Liberator", ItemEffect.LIFE_PLUGIN, [ItemEffect.BREAKOUT_SUCCEEDED], 0.5,
+        ["Liberator", new ItemEffect("Liberator", ItemEffect.LIFE_PLUGIN, [ItemEffect.BREAKOUT_SUCCEEDED], 0.4,
             "Increases points gained from breaking out players by", 4)],
 
         // Blackjack
 
-        ["Oracle", new ItemEffect("Oracle", ItemEffect.BLACKJACK_PLUGIN, [ItemEffect.WINNER_REWARD], 0.02,
-            "Increases points gained from winning blackjack by", 4)],
+        ["Oracle", new ItemEffect("Oracle", ItemEffect.BLACKJACK_PLUGIN, [ItemEffect.WINNER_REWARD], 0.05,
+            "Increases points gained from winning blackjack by")],
 
         // Russian Roulette
 
-        ["Immortal", new ItemEffect("Immortal", ItemEffect.RUSSIAN_ROULETTE_PLUGIN, [ItemEffect.BULLET_IN_CYLINDER], -0.2,
+        ["Immortal", new ItemEffect("Immortal", ItemEffect.RUSSIAN_ROULETTE_PLUGIN, [ItemEffect.BULLET_IN_CYLINDER], -0.25,
             "Reduces points lost from losing russian roulette by")],
 
-        ["Fool-Hearted", new ItemEffect("Fool-Hearted", ItemEffect.RUSSIAN_ROULETTE_PLUGIN, [ItemEffect.NO_BULLET_IN_CYLINDER], 0.15,
+        ["Fool-Hearted", new ItemEffect("Fool-Hearted", ItemEffect.RUSSIAN_ROULETTE_PLUGIN, [ItemEffect.NO_BULLET_IN_CYLINDER], 0.4,
             "Increases points gained from winning russian roulette by", 4)],
 
         // Miscellaneous
@@ -89,7 +89,7 @@ export class ItemEffect {
             ItemEffect.HORSERACE_2ND_PLACE_WINNING_SCORE_EVENT,
             ItemEffect.HORSERACE_3RD_PLACE_WINNING_SCORE_EVENT,
             ItemEffect.HORSERACE_WIN_BET_SCORE_EVENT
-        ], 0.01, "Increases all points gained by", 4)],
+        ], 0.04, "Increases all points gained by")],
 
         // Horse races
 
@@ -98,15 +98,15 @@ export class ItemEffect {
             ItemEffect.HORSERACE_2ND_PLACE_WINNING_SCORE_EVENT,
             ItemEffect.HORSERACE_3RD_PLACE_WINNING_SCORE_EVENT,
             ItemEffect.HORSERACE_WIN_BET_SCORE_EVENT
-        ], 0.04, "Increases points gained from winning horse races and bets by", 4)],
+        ], 0.08, "Increases points gained from winning horse races and bets by", 4)],
 
         // Items
 
-        ["Merchant", new ItemEffect("Merchant", ItemEffect.ITEMS_PLUGIN, [ItemEffect.ITEMS_BUY_REASON], -0.03,
-            "Receive", 4, "of the buying price back after buying an item from the shop")],
+        ["Merchant", new ItemEffect("Merchant", ItemEffect.ITEMS_PLUGIN, [ItemEffect.ITEMS_BUY_REASON], -0.075,
+            "Receive", 1, "of the buying price back after buying an item from the shop")],
 
-        ["Enhancer", new ItemEffect("Enhancer", ItemEffect.ITEMS_PLUGIN, [ItemEffect.ITEMS_UPGRADE_REASON], -0.03,
-            "Receive", 4, "of the upgrade price back after upgrading an item")],
+        ["Enhancer", new ItemEffect("Enhancer", ItemEffect.ITEMS_PLUGIN, [ItemEffect.ITEMS_UPGRADE_REASON], -0.075,
+            "Receive", 1, "of the upgrade price back after upgrading an item")],
     ]);
 
     constructor(
