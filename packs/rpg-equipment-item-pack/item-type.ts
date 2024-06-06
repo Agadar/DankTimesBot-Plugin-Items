70b0702c4927eeba81a89b34bffb1153578447c6
Agadar <martin.donkersloot@outlook.com>
2024-06-06 22:23:43 +0200

    Slightly buff twohanders to make total bonus equal to 1h+offhand

--- packs/rpg-equipment-item-pack/item-type.ts
+++ packs/rpg-equipment-item-pack/item-type.ts
@@ -4,7 +4,7 @@ export class ItemType {
 
     public static readonly ALL = new Map<string, ItemType>([
         ["One-Handed", new ItemType("One-Handed", 1.25, [EquipmentSlot.MainHand], ["Main Hand"])],
-        ["Two-Handed", new ItemType("Two-Handed", 2, [EquipmentSlot.MainHand, EquipmentSlot.OffHand], ["Two-Handed"])],
+        ["Two-Handed", new ItemType("Two-Handed", 2.1875, [EquipmentSlot.MainHand, EquipmentSlot.OffHand], ["Two-Handed"])],
         ["Off-Hand", new ItemType("Off-Hand", 0.75, [EquipmentSlot.OffHand], ["Off-Hand"])],
         ["Finger", new ItemType("Finger", 0.5, [EquipmentSlot.Fingers], ["Finger"])],
         ["Neck", new ItemType("Neck", 0.5, [EquipmentSlot.Neck], ["Neck"])],
